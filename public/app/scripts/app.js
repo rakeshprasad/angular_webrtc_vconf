0faecba966b3f7853ed0242d4435d4340dcc32e5
rakesh prasad <rword4u@gmail.com>
2015-08-04 00:03:51 +0530

    Initial commit

---
+++ public/app/scripts/app.js
@@ -0,0 +1,40 @@
+/* global angular */
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name publicApp
+ * @description
+ * # publicApp
+ *
+ * Main module of the application.
+ */
+
+angular
+  .module('publicApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/room/:roomId', {
+        templateUrl: 'app/views/room.html',
+        controller: 'RoomCtrl'
+      })
+      .when('/room', {
+        templateUrl: 'app/views/room.html',
+        controller: 'RoomCtrl'
+      })
+      .otherwise({
+        redirectTo: '/room'
+      });
+  });
+
+angular.module('publicApp')
+  .constant('config', {
+      SIGNALIG_SERVER_URL: undefined
+  });
+
+Object.setPrototypeOf = Object.setPrototypeOf || function(obj, proto) {
+  obj.__proto__ = proto;
+  return obj; 
+};
